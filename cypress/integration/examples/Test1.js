21df8b1b7139125ee3ba7d5ff75dba00e030ccde
Muthu <muthu_Mycommit>
2024-05-28 11:21:52 +0530

    Uploaded 28May2024 Morning

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -1,1 +1,22 @@
 //cypress - spec file
+///<reference types= "Cypress"/>   // to get auto suggestions of methods
+
+describe('My first test suite', function()
+{
+    it('My FirstTest Case', function(){
+
+//test step
+    cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/');
+    cy.get('.search-keyword').type('ca');
+    cy.wait(2000);
+    cy.get('.product:visible').should('have.length',4);
+
+    })
+
+//     it('My SecondTest Case', function(){
+
+// //test step
+
+
+//     })
+})
