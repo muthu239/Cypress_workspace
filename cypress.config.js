dd2ab20265fba2ddc383191a5f8079dfb6e9fa5e
Muthu <muthu_Mycommit>
2024-07-01 09:13:23 +0530

    Uploaded on 01Jul2024

--- cypress.config.js
+++ cypress.config.js
@@ -1,17 +1,24 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions:{
+    reportDir:'E:/Cypress_workspace/cypress/mochawesome-report'
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
-    // specPattern: 'cypress/integration/examples/*.js'
-    specPattern: ['cypress/integration/examples/*.js','cypress/integration/pageObjects/*.js'],  //Multiple spec files
+    specPattern: 'cypress/integration/examples/*.js',
+    // specPattern: ['cypress/integration/examples/*.js','cypress/integration/pageObjects/*.js'],  //Multiple spec files
     defaultCommandTimeout: 6000,
+    
     env: {
       url : "https://rahulshettyacademy.com",
       userID : "roronoa",
       pwd : "pwdHere"
-    }
+    },
+    
   },
 });
